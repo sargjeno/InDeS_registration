381f6efd217a5e38a8d27fe78c7534c37052d2b6
Gabor Jenofi <gabor.jenofi@flex.com>
2018-01-10 02:53:09 -0800

    [Studio] Add screen "trainingregistration"

---
+++ client/controllers/app/trainingregistration.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_trainingregistration', app_trainingregistration);
+function app_trainingregistration($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -33,5 +33,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.trainingregistration', {
+        views: {
+            app: {
+                controller: 'app_trainingregistration',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.trainingregistration');
+                }
+            }
+        }
     });
 });
