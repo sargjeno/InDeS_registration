91fde8e7916edc49fd3f5b9f00e06e1d4184c923
Gabor Jenofi <gabor.jenofi@flex.com>
2018-01-11 01:22:22 -0800

    [Extension] Update application settings

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
     // Start URL that browsing should start at.
-    startURL: 'http://itiner.tab.flextronics.com:8080/Itiner/',
+    startURL: 'http://itiner.tab.flextronics.com:8080/Itiner/en/IDLTrainingSystem/IDLTrainingMatrixRealization/trainingregistrationinbatch',
     // Start Method when there is no start URL.  This is fpr when we are doing an API integration,
     // and we don't actually need to load phantom (yet).
     // startMethod: "login.onStart",
