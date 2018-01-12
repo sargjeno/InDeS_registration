822e1d24b82be9c2d7a081cc5bb8a7046486dd61
Gabor Jenofi <gabor.jenofi@flex.com>
2018-01-12 06:02:07 -0800

    [Studio] Modified screen "trainingregistration"

--- client/controllers/app/trainingregistration.js
+++ client/controllers/app/trainingregistration.js
@@ -7,19 +7,18 @@ function app_trainingregistration($scope, app) {
         
     // $scope.clickWhat = $scope.data.realization;
     
+    // console.log(app.data.realizationListDropdown.options['+selectedId+'].label);
+    // console.log("realizationDropDown: " + app.data.realizationListDropdown.options[selectedId].label);
+    // alert('Why Not Work!!');    
+
+    // var selectedId = $scope.data.realization.$i;
+    // var actionUrl = 'realizationListDropdown.options['+selectedId+'].clicked';    
+
+    // app.data.realizationListDropdown.options[selectedId].clicked;
+        
     console.log($scope.data.realization.$i);
     console.log($scope.data.realization.label);
-    
-    var selectedId = $scope.data.realization.$i;
-    var actionUrl = 'realizationListDropdown.options['+selectedId+'].clicked';
-    console.log(actionLabel);
-    
-    // console.log(app.data.realizationListDropdown.options['+selectedId+'].label);
 
-    console.log("realizationDropDown: " + app.data.realizationListDropdown.options[selectedId].label);
-    // alert('Why Not Work!!');    
-    
-    // app.data.realizationListDropdown.options[selectedId].clicked;
     app.action('trainingregistration', $scope.data.realization.clicked);
     
     // app.action('trainingregistration', 'realizationListDropdown.options['+$scope.data.realization.$i+'].clicked');
