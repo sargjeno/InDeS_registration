71695928e6d1f3d82a3e8bb8223df18de1d36563
Gabor Jenofi <gabor.jenofi@flex.com>
2018-01-12 02:14:53 -0800

    [Studio] Modified screen "trainingregistration"

--- client/controllers/app/trainingregistration.js
+++ client/controllers/app/trainingregistration.js
@@ -2,12 +2,12 @@ angular.module('app').controller('app_trainingregistration', app_trainingregistr
 function app_trainingregistration($scope, app) {
     'use strict';
     app.init($scope);
+    
     $scope.clickSelectedRealization = function(){
-    console.log($scope.data.realization);
     console.log($scope.data.realization.$i);
     console.log($scope.data.realization.label);
     // $scope.clickWhat = $scope.data.realization;
-    app.action('trainingregistration', 'realizationListDropdown.options["2"].clicked');
+    app.action('trainingregistration', 'realizationListDropdown.options[2].clicked');
     // app.action('trainingregistration', 'realizationListDropdown.options['+$scope.data.realization.$i+'].clicked');
     };
 }
